dbb002452517fba8c841928b3967d168ddf9fe1f
missi0415 <missi0415@gmail.com>
2021-06-23 22:10:43 +0900

    埋め込みif分

--- work/web/index.php
+++ work/web/index.php
@@ -2,7 +2,8 @@
 
 require("../app/functions.php");
 
-$name = "Taro <script>alert(1);</script>";
+$names = [
+];
 
 ?>
 
@@ -13,7 +14,14 @@ $name = "Taro <script>alert(1);</script>";
   <title>PHP Practice</title>
 </head>
 <body>
-  <p>Hello,<?= h($name); ?>!</p>
-
+  <ul>
+    <?php if (empty($names)) { ?>
+      <li>Nobody!</li>
+    <?php } else { ?>
+      <?php foreach ($names as $name){ ?>
+        <li><?= h($name); ?></li>
+      <?php } ?>
+    <?php } ?>  
+  </ul> 
 </body>
 </html>
