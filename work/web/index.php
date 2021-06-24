5d644495107da8ecacfbaa48e77dec68dcff6d4c
missi0415 <missi0415@gmail.com>
2021-06-24 12:37:11 +0900

    formの値の取得

--- work/web/index.php
+++ work/web/index.php
@@ -5,6 +5,10 @@ require("../app/functions.php");
 include("../app/_parts/_header.php");
 
 ?>
+<form action = "result.php" method="get">
+  <input type="text" name="message">
+  <button>Send</button>
+</form>
 
 
 
