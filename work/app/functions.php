57e967e4c854f4b91d67be898a872335b0e0216c
missi0415 <missi0415@gmail.com>
2021-06-25 20:59:36 +0900

    takenの使用

--- work/app/functions.php
+++ work/app/functions.php
@@ -5,3 +5,24 @@ function h($str)
 {
   return htmlspecialchars($str,ENT_QUOTES, "UTF-8");
 }
+
+function createToken()
+{
+  if (!isset($_SESSION["token"])){
+    $_SESSION["token"] = bi2hex(random_bytes(32));
+    //random_bytes(引数)ランダムな文字列の生成
+    
+  }
+}
+
+function validateToken()
+{
+  if (
+    empty($_SESSION["token"]) ||
+    $_SESSION["token"] !== filter_input(INPUT_POST,"token")
+  ){
+    exit("Invalid post request");
+  }
+}
+
+session_start();
