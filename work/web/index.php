6c97d74a93976a5d12a905111610a2cefc15ef72
missi0415 <missi0415@gmail.com>
2021-06-25 15:47:09 +0900

    get post 違い

--- work/web/index.php
+++ work/web/index.php
@@ -2,12 +2,20 @@
 
 require("../app/functions.php");
 
-// $color = filter_input(INPUT_COOKIE, "color") ?? "transparent";
+$filename = "../app/messages.txt";
+$messages = file($filename,FILE_IGNORE_NEW_LINES);
 
 include("../app/_parts/_header.php");
 
 ?>
-<form action = "result.php" method="post">
+<ul>
+<?php foreach ($messages as $message): ?>
+  <li><?= h($message); ?></li>
+<?php endforeach; ?>
+</ul>
+
+<form action= "result.php" method="post">
+  <input type= "text" name="message">
   <button>Post</button>
 </form>
 
