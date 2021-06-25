63615c578171e354af08dbc128d1e321394f6e2f
missi0415 <missi0415@gmail.com>
2021-06-25 15:55:32 +0900

    掲示板の作成

--- work/web/result.php
+++ work/web/result.php
@@ -2,11 +2,19 @@
 
 require("../app/functions.php");
 
+
+$message = trim(filter_input(INPUT_POST,"message"));
+$message = $message !== "" ? $message : "...";
+$filename = "../app/messages.txt";
+$fp = fopen($filename, "a");
+fwrite($fp,$message . "\n");
+fclose($fp);
+
 include("../app/_parts/_header.php");
 
 ?>
-
-<p><a href = "index.php">Go back</a></p>
+  <p>Message added!</p>
+  <p><a href = "index.php">Go back</a></p>
 
 
 <?php
